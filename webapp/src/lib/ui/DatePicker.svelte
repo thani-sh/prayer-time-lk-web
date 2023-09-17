60c75d80ce622678902d82dfcf3b37398c183db2
Thanish Nizam <mnmtanish@gmail.com>
2023-09-17 22:47:24 +0200

    Add a date picker on the UI

---
+++ webapp/src/lib/ui/DatePicker.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+	import { date } from '$lib/store';
+
+	function toDateString(input: Date) {
+		const m = '' + (input.getMonth() + 1);
+		const d = '' + input.getDate();
+		const y = input.getFullYear();
+		return [y, m.padStart(2, '0'), d.padStart(2, '0')].join('-');
+	}
+
+	let dateString = toDateString($date);
+
+	$: {
+		date.set(new Date(dateString));
+	}
+</script>
+
+<input class="datepicker" type="date" bind:value={dateString} />
+
+<style>
+	.datepicker {
+		padding: 10px 15px;
+		background: none;
+		border: none;
+		outline: none;
+		color: #888;
+		box-sizing: border-box;
+		color-scheme: dark;
+	}
+
+	.datepicker:focus {
+		border: none;
+		outline: none;
+	}
+</style>
--- webapp/src/routes/+page.svelte
+++ webapp/src/routes/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { date } from '$lib/store';
 	import TimeTable from '$lib/ui/TimeTable.svelte';
+	import Datepicker from '$lib/ui/DatePicker.svelte';
 	import { forDate } from '@thani-sh/prayer-time-lk';
 
 	$: prayers = forDate($date);
@@ -10,6 +11,9 @@
 	<h1>Prayer Time</h1>
 	<TimeTable {prayers} />
 </div>
+<div class="overlay">
+	<Datepicker />
+</div>
 
 <style>
 	.content {
@@ -17,11 +21,23 @@
 		margin-top: -100px;
 		flex-direction: column;
 		font-size: 24px;
-		pointer-events: none;
 	}
 
 	.content h1 {
 		display: block;
 		text-align: center;
+	}
+
+	.overlay {
+		position: absolute;
+		display: flex;
+		box-sizing: border-box;
+		padding: 50px;
+		align-items: flex-end;
+		justify-content: center;
+		width: 100%;
+		height: 100vh;
+		top: 0;
+		left: 0;
 	}
 </style>
